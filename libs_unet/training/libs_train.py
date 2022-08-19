033a7c632adb995083e49bc9b07f49157b06b571
Matthew Goff <mattgoffgss@gmail.com>
2022-08-19 16:15:26 -0700

    added model weights tracking feature

--- libs_unet/training/libs_train.py
+++ libs_unet/training/libs_train.py
@@ -1,8 +1,10 @@
-#imports
 import torch
+from libs_unet.training.model_delta import state_diff
 
-def train_loop(dataloader, model, loss_fn, optimizer):
+def train_loop(dataloader, model, loss_fn, optimizer, debug=False, nodes=[]):
     size = len(dataloader.dataset)
+    #capture starting state of model for difference reporting
+    init_dict = model.state_dict()
     model.train()
     for batch, (X, y) in enumerate(dataloader):
         # Compute prediction and loss
@@ -17,6 +19,18 @@ def train_loop(dataloader, model, loss_fn, optimizer):
         if batch % 10 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:.7E}  [{current:>5d}/{size:>5d}]")
+            if debug == True:
+                update_dict = state_diff(model.state_dict(), init_dict)
+                #print(update_dict)
+                if len(nodes) == 0:
+                    for key,value in update_dict.items():
+                        print(f"{key}:\n{value}")
+                else:
+                    for key,value in update_dict.items():
+                        if key in nodes:
+                            print(f"{key}:\n{value}")
+                            
+                init_dict = model.state_dict()
 
 
 def test_loop(dataloader, model, loss_fn):
---
+++ libs_unet/training/model_delta.py
@@ -0,0 +1,51 @@
+import torch
+
+def state_diff(new_dict, old_dict, type = 'model', verbose=False):
+
+    """ Function to evaluate changes to tunable model parameters during training.
+        Arguments:
+            new_dict: current state dictionary
+            old_dict: prior state dictionary
+            type = 'model'|'optimizer'
+            verbose = True gives maximum new/old tensor detail in addition to summary
+            nodes = [], optional string list of specific nodes to report on
+        Returns:
+            model: dict with same key structure as inputs, with summary and detail tensors
+                summary for each key: size(3,3) 
+                dim0: new, old, delta
+                dim1: meanabsval, range, var
+                #note delta is element-wise subtraction new-old
+            optimizer: dict of dict with var_name:{new:value, old:value}
+    """
+    
+    results_dict = {}
+    match type:
+        case 'model':
+            #calculate the corresponding dictionary of element-wise changes
+            diff_dict = {}
+            for key,value in new_dict.items():
+                diff_dict[key] = value - old_dict[key]
+
+            #going to populate a node dict with tensors of results
+            #process node by node in the model across all three dicts
+            for node in new_dict: #iterate keys, order maintained since python 3.6
+              summary = torch.zeros(3,3)
+              summary[0,0] = torch.mean(torch.abs(new_dict[node])).item()
+              summary[0,1] = torch.max(new_dict[node]).item() - torch.min(new_dict[node]).item()
+              summary[0,2] = torch.var(new_dict[node]).item()
+              summary[1,0] = torch.mean(torch.abs(old_dict[node])).item()
+              summary[1,1] = torch.max(old_dict[node]).item() - torch.min(old_dict[node]).item()
+              summary[1,2] = torch.var(old_dict[node]).item()
+              summary[2,0] = torch.mean(torch.abs(diff_dict[node])).item()
+              summary[2,1] = torch.max(diff_dict[node]).item() - torch.min(diff_dict[node]).item()
+              summary[2,2] = torch.var(diff_dict[node]).item()
+              results_dict[node] = summary
+            return results_dict
+        
+        case 'optimizer':
+            for key, value in new_dict:
+                results_dict[key] = {"new":value, "old":old_dict[key]}
+            return results_dict
+        
+        case _:
+            raise ValueError(f"Invalid type parameter {type} provided")
