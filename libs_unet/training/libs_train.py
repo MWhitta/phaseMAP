a5eb1472f00471e77d2bd9db296ab89d0c764e5d
Matthew Goff <mattgoffgss@gmail.com>
2022-08-22 11:47:23 -0700

    tensorboard for loss

--- libs_unet/training/libs_train.py
+++ libs_unet/training/libs_train.py
@@ -1,10 +1,13 @@
 import torch
 from libs_unet.training.model_delta import state_diff
 
-def train_loop(dataloader, model, loss_fn, optimizer, debug=False, nodes=[]):
+
+def train_loop(dataloader, model, loss_fn, optimizer, writer, epoch, debug=False, nodes=[]):
+    # number of batches
     size = len(dataloader.dataset)
     #capture starting state of model for difference reporting
     init_dict = model.state_dict()
+
     model.train()
     for batch, (X, y) in enumerate(dataloader):
         # Compute prediction and loss
@@ -17,8 +20,9 @@ def train_loop(dataloader, model, loss_fn, optimizer, debug=False, nodes=[]):
         optimizer.step() #leverages tensor gradients from backward()
 
         if batch % 10 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:.7E}  [{current:>5d}/{size:>5d}]")
+            loss, current = loss.item(), epoch * batch * len(X)
+            writer.add_scalar("Loss/train", loss, current)
+            #print(f"loss: {loss:.7E}  [{current:>5d}/{size:>5d}]")
             if debug == True:
                 update_dict = state_diff(model.state_dict(), init_dict)
                 #print(update_dict)
@@ -33,16 +37,16 @@ def train_loop(dataloader, model, loss_fn, optimizer, debug=False, nodes=[]):
                 init_dict = model.state_dict()
 
 
-def test_loop(dataloader, model, loss_fn):
+def test_loop(dataloader, model, loss_fn, writer, epoch):
     num_batches = len(dataloader)
-    test_loss, correct = 0, 0
+    test_loss = 0
     model.eval()
     with torch.no_grad():
         for X, y in dataloader:
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
 
-    test_loss /= num_batches #should be a mean of means
-    
-    print(f"Test Error: \n Avg loss: {test_loss:.7E} \n")
+    test_loss /= num_batches #should be a mean of batch means
+    writer.add_scalar("Loss/valid", test_loss, epoch)
+    #print(f"Test Error: \n Avg loss: {test_loss:.7E} \n")
     return test_loss
